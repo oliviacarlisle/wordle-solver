08d114101905e5ef3e98ad5b49c6ccb610c8ded7
Olivia Carlisle <151247171+oliviacarlisle@users.noreply.github.com>
2024-10-29 10:41:31 -0400

    Add validation logic

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 // Define types
 export type Feedback = Uint8Array;
+export type GuessInfo = { word: string; feedback: number[] };
 export type GuessWithFeedback = { word: string; feedback: Feedback }; // 1 for green, 2 for yellow, 0 for gray
